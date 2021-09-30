e0230939cbe1131c96e8c3177bef04da8888ff34
dacuff <dacuff@appspace.com>
2021-09-30 14:25:44 -0400

    latest changes

---
+++ src/templates/Hero.tsx
@@ -0,0 +1,48 @@
+import Link from 'next/link';
+
+import { Background } from '../background/Background';
+import { Button } from '../button/Button';
+import { HeroOneButton } from '../hero/HeroOneButton';
+import { Section } from '../layout/Section';
+import { NavbarTwoColumns } from '../navigation/NavbarTwoColumns';
+import { Logo } from './Logo';
+
+const Hero = () => (
+  <Background color="bg-gray-100">
+    <Section yPadding="py-6">
+      <NavbarTwoColumns logo={<Logo xl />}>
+        <li>
+          <Link href="#">
+            <a>Link</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/">
+            <a>Another Link</a>
+          </Link>
+        </li>
+      </NavbarTwoColumns>
+    </Section>
+
+    <Section yPadding="pt-20 pb-32">
+      <HeroOneButton
+        title={
+          <>
+            {'Crystal Shine\n'}
+            <span className="text-primary-500">Specialty Cleaning Service</span>
+          </>
+        }
+        description="Serving the greater Danbury, Brookfield, Newtown, Easton, Redding & Ridgefield areas of Connecticut"
+        button={
+          <Link href="#">
+            <a>
+              <Button xl>Contact Us</Button>
+            </a>
+          </Link>
+        }
+      />
+    </Section>
+  </Background>
+);
+
+export { Hero };
