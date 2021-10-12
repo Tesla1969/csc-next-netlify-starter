c79b349f3a0f3084aa3c63eae556f9a430d26050
dacuff <dacuff@appspace.com>
2021-10-12 09:22:18 -0400

    css

--- src/templates/Hero.tsx
+++ src/templates/Hero.tsx
@@ -8,7 +8,7 @@ import { NavbarTwoColumns } from '../navigation/NavbarTwoColumns';
 import { Logo } from './Logo';
 
 const Hero = () => (
-  <Background color="cs-pink">
+  <Background color="cs-blue" bgclass="bg-pink">
     <Section yPadding="py-6">
       <NavbarTwoColumns logo={<Logo xl />}>
         <li>
