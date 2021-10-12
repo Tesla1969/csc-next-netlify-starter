16949c1d767dae01430169b013fae1011a1d91ce
dacuff <dacuff@appspace.com>
2021-10-12 11:54:00 -0400

    comment out section

--- src/templates/Hero.tsx
+++ src/templates/Hero.tsx
@@ -4,12 +4,10 @@ import { Background } from '../background/Background';
 import { Button } from '../button/Button';
 import { HeroOneButton } from '../hero/HeroOneButton';
 import { Section } from '../layout/Section';
-import { NavbarTwoColumns } from '../navigation/NavbarTwoColumns';
-import { Logo } from './Logo';
 
 const Hero = () => (
   <Background color="cs-blue" bgclass="bg-best-cleaning">
-    <Section yPadding="py-6">
+    {/* <Section yPadding="py-6">
       <NavbarTwoColumns logo={<Logo xl />}>
         <li>
           <Link href="#">
@@ -22,7 +20,7 @@ const Hero = () => (
           </Link>
         </li>
       </NavbarTwoColumns>
-    </Section>
+    </Section> */}
 
     <Section yPadding="pt-20 pb-32">
       <HeroOneButton
