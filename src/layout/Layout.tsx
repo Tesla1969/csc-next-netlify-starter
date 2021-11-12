6782f86cbb59a08f51240a293f616040819e516d
dacuff <dacuff@appspace.com>
2021-11-12 11:22:02 -0500

    trying to setup resusable layout

---
+++ src/layout/Layout.tsx
@@ -0,0 +1,18 @@
+import React, { ReactNode } from 'react';
+
+type Props = {
+  children?: ReactNode;
+  title?: string;
+};
+
+const Layout = ({ children, title = 'This is the default title' }: Props) => {
+  return (
+    <>
+      <div>
+        <span>{title}</span>
+        {children}
+      </div>
+    </>
+  );
+};
+export default Layout;
--- src/templates/Base.tsx
+++ src/templates/Base.tsx
@@ -1,15 +1,18 @@
+import React from 'react';
+
+import Layout from '../layout/Layout';
 import { Meta } from '../layout/Meta';
 import { AppConfig } from '../utils/AppConfig';
 import { Banner } from './Banner';
 import { Footer } from './Footer';
 import { Hero } from './Hero';
-import { VerticalFeatures } from './VerticalFeatures';
 
 const Base = () => (
   <div className="antialiased">
     <Meta title={AppConfig.title} description={AppConfig.description} />
     <Hero />
-    <VerticalFeatures />
+    <Layout title="hello next js" />
+
     <Banner />
     <Footer />
   </div>
