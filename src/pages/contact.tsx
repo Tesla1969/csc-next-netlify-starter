25e27221aee9e2fecd12aac7e1f78b3fe778c987
YsfSt <ysfst2@gmail.com>
2021-11-17 00:17:26 +0100

    fix formated orders

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+
 import Form from '../form/Form';
 import Layout from '../layout/Layout';
 import SubHeader from '../subHeader/SubHeader';
+
 import { Footer } from '../templates/Footer';
 import { Hero } from '../templates/Hero';
 
